e944de64d678d7441ad9c4e1013c575f50ad9da1
Stefan Beyer <sbeyer@threatint.com>
2020-12-16 10:23:50 +0100

    minor addition, updated 3rd-party

--- fqdn_test.go
+++ fqdn_test.go
@@ -15,6 +15,8 @@ func TestIsFqdn(t *testing.T) {
 		{"www.de.company.com", true},
 		{"www.de.company.com/newsticker", false},
 		{"1.2.3.4", false},
+		{"612050612050612050612050612050612050-dot-onk89909.wn.r.appspot.com", true},
+		{"9876543456886756565656-secondary.z19.web.core.windows.net", true},
 	}
 
 	for _, e := range hostTests {
